f4fe9906dfd99131325a619de8fab4c3db51d263
mystevotgit <oyeolatunji@gmail.com>
2020-10-17 16:25:09 +0100

    Fetching data from Apis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [text, setText] = useState(input);
+  const [data, setData] = useState([]);
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    setText(input);
+    console.log(text);
+  }
+  
+  const api = "https://geo.ipify.org/api/v1?apiKey=at_LiGqZ4wuXR3UoGUUCLa6dd5CB8UjE&ipAddress=" + text;
+
+  useEffect(() => {
+    async function getData() {
+      const result = await axios(api);
+      setData(result.data)
+    }
+    getData()
+  }, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={input} onChange={e=> setInput(e.target.value)}/>
+                <input type="submit" value="Track" />
+            </form>
+      </div>
+      <div>
+      <p>IP ADDRESS: {data.ip}  LOCATION: {data.location?.city}, {data.location?.country}   {data.location?.postalCode}   TIMEZONE: UTC {data.location?.timezone}   ISP: {data.isp}</p>  
+      </div>
+      <div>
+
+        IP Address Tracker
+
+        Search for any IP address or domain
+
+        {/* IP Address
+        Location
+        Timezone
+          UTC add offset value dynamically using the API
+        ISP */}
+        
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="https://github.com/mystevotgit">Olatunji Oye</a>.
+        </div>
+      </div>
+    </>
   );
 }
 
