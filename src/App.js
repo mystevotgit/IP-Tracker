75be3bd07b819447860dfe2caca71fdb0f627c60
mystevotgit <oyeolatunji@gmail.com>
2020-10-18 15:56:49 +0100

    IP-TRACKER completed

--- src/App.js
+++ src/App.js
@@ -1,58 +1,62 @@
 import React, {useEffect, useState} from 'react';
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import pattern from './pattern-bg.png'
 import axios from 'axios';
-import './App.css';
+import Data from './Data';
+import "./App.css";
 
-function App() {
+export default function App() {
   const [input, setInput] = useState("");
   const [text, setText] = useState(input);
   const [data, setData] = useState([]);
-  
+  const [loc, setLoc] = useState([45.4, -75.7]);
+  const [showPopUp, setShowPopUp] = useState(false);
   function handleSubmit(e) {
     e.preventDefault();
     setText(input);
-    console.log(text);
   }
-  
   const api = "https://geo.ipify.org/api/v1?apiKey=at_LiGqZ4wuXR3UoGUUCLa6dd5CB8UjE&ipAddress=" + text;
-
   useEffect(() => {
     async function getData() {
       const result = await axios(api);
       setData(result.data)
+      setLoc([result.data.location?.lat, result.data.location?.lng])
+      console.log(result.data);
     }
     getData()
   }, [text]);
-
   return (
-    <>
-      <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={input} onChange={e=> setInput(e.target.value)}/>
-                <input type="submit" value="Track" />
-            </form>
-      </div>
-      <div>
-      <p>IP ADDRESS: {data.ip}  LOCATION: {data.location?.city}, {data.location?.country}   {data.location?.postalCode}   TIMEZONE: UTC {data.location?.timezone}   ISP: {data.isp}</p>  
-      </div>
-      <div>
-
-        IP Address Tracker
-
-        Search for any IP address or domain
-
-        {/* IP Address
-        Location
-        Timezone
-          UTC add offset value dynamically using the API
-        ISP */}
-        
-        <div className="attribution">
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-          Coded by <a href="https://github.com/mystevotgit">Olatunji Oye</a>.
+    <div>
+      <div className="top-container">
+      <h1 className="title">IP Address Tracker</h1>
+        <form className="top-form" onSubmit={handleSubmit}>
+          <input className="input" type="text" value={input} onChange={e=> setInput(e.target.value)}/>
+          <input className="submit" type="submit" value="  >  " />
+        </form>
+        <img className="top-image" src={pattern} alt="Logo"/>
+        <div className="display">
+          <Data title = "IP ADDRESS" info = {data.ip} />
+          <Data title = "LOCATION" info = {data.location?.city +', ' + data.location?.country +' '+ data.location?.postalCode}/>
+          <Data title = "TIMEZONE" info = {data.location?.timezone} />
+          <Data title = "ISP" info = {data.isp} />
         </div>
       </div>
-    </>
+      <Map center={loc} zoom={13}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+          <Marker onMouseOver={()=>setShowPopUp(true)} onMouseOut={()=>setShowPopUp(false)} position={[loc[0], loc[1]]}/>
+          {
+    
+          showPopUp && (<Popup position={[loc[0], loc[1]]}>
+            <div>
+              <p>{data.as?.domain}</p>
+            </div>
+          </Popup>)
+    
+          }
+      </Map>
+    </div>
   );
 }
-
-export default App;
