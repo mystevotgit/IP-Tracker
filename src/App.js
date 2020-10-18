321e710a4ce35202f9c05ff045f0ccc214fb5913
mystevotgit <oyeolatunji@gmail.com>
2020-10-18 21:53:30 +0100

    IP-TRACKER modified

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
   const [data, setData] = useState([]);
   const [loc, setLoc] = useState([45.4, -75.7]);
   const [showPopUp, setShowPopUp] = useState(false);
+  const [error, setError] = useState(null);
   function handleSubmit(e) {
     e.preventDefault();
     setText(input);
@@ -18,10 +19,14 @@ export default function App() {
   const api = "https://geo.ipify.org/api/v1?apiKey=at_LiGqZ4wuXR3UoGUUCLa6dd5CB8UjE&ipAddress=" + text;
   useEffect(() => {
     async function getData() {
-      const result = await axios(api);
-      setData(result.data)
-      setLoc([result.data.location?.lat, result.data.location?.lng])
-      console.log(result.data);
+      try{
+        const result = await axios(api);
+        setData(result.data);
+        setError(null)
+        setLoc([result.data.location?.lat, result.data.location?.lng])
+      }catch( {response:{data}} ){
+        setError(data);
+      }
     }
     getData()
   }, [text]);
@@ -29,13 +34,15 @@ export default function App() {
     <div>
       <div className="top-container">
       <h1 className="title">IP Address Tracker</h1>
+      <h5 className="subtitle" >Get an IP location (e.g 8.8.8.8). Default IP is your current network IP.</h5>
         <form className="top-form" onSubmit={handleSubmit}>
-          <input className="input" type="text" value={input} onChange={e=> setInput(e.target.value)}/>
+          <input className="input" type="text" placeholder="Enter an IP address" value={input} onChange={e=> setInput(e.target.value)}/>
           <input className="submit" type="submit" value="  >  " />
         </form>
+        <h3 className="error" >{error?.messages}</h3>
         <img className="top-image" src={pattern} alt="Logo"/>
         <div className="display">
-          <Data title = "IP ADDRESS" info = {data.ip} />
+          <Data title = "CURRENT IP ADDRESS" info = {data.ip} />
           <Data title = "LOCATION" info = {data.location?.city +', ' + data.location?.country +' '+ data.location?.postalCode}/>
           <Data title = "TIMEZONE" info = {data.location?.timezone} />
           <Data title = "ISP" info = {data.isp} />
@@ -52,6 +59,7 @@ export default function App() {
           showPopUp && (<Popup position={[loc[0], loc[1]]}>
             <div>
               <p>{data.as?.domain}</p>
+              <p>{data.location?.region}</p>
             </div>
           </Popup>)
     
