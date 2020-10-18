fe80d20be99024ac7799cea7d9e2d5609ab103bb
mystevotgit <oyeolatunji@gmail.com>
2020-10-18 19:53:34 +0100

    Updates

---
+++ precache-manifest.34c467d398592cda4b3f4acf6ad5088c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c30dfbe7e208a1209f80fed9f0014f",
+    "url": "/ip-tracker/index.html"
+  },
+  {
+    "revision": "32c733582b4fb31f3f8d",
+    "url": "/ip-tracker/static/css/main.18cef5b0.chunk.css"
+  },
+  {
+    "revision": "2aa1dca1c11e00089d52",
+    "url": "/ip-tracker/static/js/2.a684179a.chunk.js"
+  },
+  {
+    "revision": "ce3126c332c3bc8f1544c88c174f7c51",
+    "url": "/ip-tracker/static/js/2.a684179a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32c733582b4fb31f3f8d",
+    "url": "/ip-tracker/static/js/main.1dffba38.chunk.js"
+  },
+  {
+    "revision": "6fc0aa65a066ea435689",
+    "url": "/ip-tracker/static/js/runtime-main.8dfc87e8.js"
+  },
+  {
+    "revision": "8d3f4ad64bf3a9320e172edf01e0c016",
+    "url": "/ip-tracker/static/media/pattern-bg.8d3f4ad6.png"
+  }
+]);
